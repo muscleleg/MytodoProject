b9427c9194e1a1be0bbc1146af44d61a1f78c628
muscleleg <aamoof@gmail.com>
2022-09-04 18:10:56 +0900

    todolist 프론트 작업중

--- src/main/resources/static/js/todolist.js
+++ src/main/resources/static/js/todolist.js
@@ -1,13 +1,20 @@
 
+
 const todolistbutton=document.querySelector("div.todolist__sidebar__todolistbutton");
 const searchbutton=document.querySelector("div.todolist__sidebar__searchbutton");
 const sidebarpast=document.querySelector("div.todolist__sidebarpast");
 const sidebarsearch=document.querySelector("div.todolist__sidebarsearch");
+const todolist__contentcontainerForm=document.querySelector("div.todolist__contentcontainer__form");
+const todolist__addbutton=document.querySelector("div.todolist__contentcontainer__todolist__addbutton");
+const todolist__formcancelbutton=document.querySelector("div.todolist__contentcontainer__form__buttons__cancel");
+//=================달력날짜 현재 날짜로 변경=========================//
 Date.prototype.toDateInputValue = (function() {
     var local = new Date(this);
     local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
     return local.toJSON().slice(0,10);
 });
+document.getElementById('date').value = new Date().toDateInputValue();
+//================================================================//
 function handle_button_Enter(b){
     // b.style.backgroundColor ="#a9a9a9";
     b.classList.toggle("buttonEnter");
@@ -26,7 +33,18 @@ function handle_button_click(b1,b2,s1,s2){
     s2.classList.remove("visible");
 
 }
+function handle_button_clickForm(form){
+    form.classList.toggle("formvisible");
 
+}
+function handle_button_cancelForm(form){
+    form.classList.toggle("formvisible");
+    const textarea = document.querySelector("div.todolist__contentcontainer__form textarea");
+    textarea.value = "";
+}
+/**
+ * 사이드바 조작 js
+ */
 // todolistbutton.addEventListener("mouseenter",() =>handle_button_Enter(todolistbutton));
 // todolistbutton.addEventListener("mouseleave",() =>handle_button_leave(todolistbutton));
 todolistbutton.addEventListener("click",() =>handle_button_click(todolistbutton,searchbutton,sidebarpast,sidebarsearch));
@@ -35,7 +53,12 @@ todolistbutton.addEventListener("click",() =>handle_button_click(todolistbutton,
 // searchbutton.addEventListener("mouseenter",() =>handle_button_Enter(searchbutton));
 // searchbutton.addEventListener("mouseleave",() =>handle_button_leave(searchbutton));
 searchbutton.addEventListener("click",() =>handle_button_click(searchbutton,todolistbutton,sidebarsearch,sidebarpast));
-document.getElementById('date').value = new Date().toDateInputValue();
 
 
 // h1.addEventListener("click",handleTitleClick);
+
+/**
+ * form 조작 js
+ */
+ todolist__addbutton.addEventListener("click",() =>handle_button_clickForm(todolist__contentcontainerForm));
+ todolist__formcancelbutton.addEventListener("click",() =>handle_button_cancelForm(todolist__contentcontainerForm));
